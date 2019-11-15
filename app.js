8c02277b70649401bcbe4c46332282f030516e91
Elias Holter <eliasholter1@gmail.com>
2019-11-15 14:12:41 -0600

    Fixed Broken Code

--- app.js
+++ app.js
@@ -202,15 +202,26 @@ function searchByInfo(information,people){
   selectFromInformationSearchMatches(possibleMatches,people);
   }
 }
-function selectFromInformationSearchMatches(possibleMatches,people){
-let matchesToDisplayArray=[];
-let matchesToDisplayString="";  
-for (let i=0;i<possibleMatches.length;i++){
-matchesToDisplayArray[i]=(i+1)+") "+possibleMatches[i].firstName+" "+possibleMatches[i].lastName+"\n";
-}
-matchesToDisplayString=matchesToDisplayArray.join("");
-prompt("Please input the number that matches the result you'd like to further inspect:\n"+matchesToDisplayString);
 
+function selectFromInformationSearchMatches(possibleMatches,people){
+  let matchesToDisplayArray=[];
+  let matchesToDisplayString="";  
+
+  for (let i=0;i<possibleMatches.length;i++){
+    matchesToDisplayArray[i]=(i+1)+") "+possibleMatches[i].firstName+" "+possibleMatches[i].lastName+"\n";
+  }
+
+  matchesToDisplayString=matchesToDisplayArray.join("");
+  let userResultChoice=0
+  userResultChoice=prompt("Please input the number that matches the result you'd like to further inspect:\n"+matchesToDisplayString);
+  let userResultChoiceNumber=parseInt(userResultChoice);
+
+  if (userResultChoiceNumber<1||userResultChoiceNumber>(possibleMatches.length)||Number.isNaN(userResultChoiceNumber)){
+      selectFromInformationSearchMatches(possibleMatches,people);
+  }
+  else  {
+      mainMenu(possibleMatches[userResultChoiceNumber-1], people);
+  }
 }
 
 function displayFamily(targetPerson, people){
